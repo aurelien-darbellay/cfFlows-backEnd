32aa750085ff747ada009d5b7c087b05c1002762
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-05-26 14:18:44 +0200

    deleted exception class I'm not using anymore

--- src/main/java/s05t02/interactiveCV/exception/IllegalDocumentTypeException.java
+++
@@ -1,7 +0,0 @@
-package s05t02.interactiveCV.exception;
-
-public class IllegalDocumentTypeException extends Exception {
-    public IllegalDocumentTypeException() {
-        super("Error: document type not recognized");
-    }
-}
