eef056083e30dc25835c87e2865bbb7e1cd4fed2
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-07-04 18:11:09 +0200

    included username and authorities in login response

--- src/main/java/s05t02/interactiveCV/service/security/jwt/JwtCookieSuccessHandler.java
+++ src/main/java/s05t02/interactiveCV/service/security/jwt/JwtCookieSuccessHandler.java
@@ -1,11 +1,17 @@
 package s05t02.interactiveCV.service.security.jwt;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.server.WebFilterExchange;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
@@ -13,12 +19,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class JwtCookieSuccessHandler implements ServerAuthenticationSuccessHandler {
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     private final JwtUtils jwtUtils;
     private static final Logger log = LoggerFactory.getLogger(JwtCookieSuccessHandler.class);
@@ -29,9 +40,20 @@ public class JwtCookieSuccessHandler implements ServerAuthenticationSuccessHandl
         ServerWebExchange exchange = webFilterExchange.getExchange();
         Jwt jwt = createJwtCookie(exchange, authentication);
         log.atDebug().log("Jwt created :" + jwt.getClaims().toString());
-        // You can redirect or just complete response here
-        exchange.getResponse().setStatusCode(HttpStatus.OK);
-        return exchange.getResponse().setComplete();
+        Map<String, Object> bodyMap = Map.of(
+                "username", ((UserDetails) authentication.getPrincipal()).getUsername(),
+                "authorities", authentication.getAuthorities()
+        );
+        String bodyJson;
+        try {
+            bodyJson = objectMapper.writeValueAsString(bodyMap);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        ServerHttpResponse response = exchange.getResponse();
+        DataBuffer buffer = response.bufferFactory().wrap(bodyJson.getBytes(StandardCharsets.UTF_8));
+        response.setStatusCode(HttpStatus.OK);
+        return response.writeWith(Mono.just(buffer));
     }
 
     public Jwt createJwtCookie(ServerWebExchange exchange, Authentication authentication) {
